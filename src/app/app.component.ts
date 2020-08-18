0ffdefe0fb83d2be7f4382dbcbd0aee681077703
sarvesh-19 <technicalguy555@gmail.com>
2020-08-17 22:22:13 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+   
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngDataTable';
+export class AppComponent implements OnInit{
+  title = 'datatables';
+  dtOptions: DataTables.Settings = {};
+  posts;
+  
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true
+    };
+  
+    this.http.get('http://jsonplaceholder.typicode.com/posts')
+      .subscribe(posts => {
+        this.posts = posts;
+    });
+  
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+  
 import { AppComponent } from './app.component';
-
+  
+import { DataTablesModule } from 'angular-datatables';
+import { HttpClientModule } from '@angular/common/http';
+  
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DataTablesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
